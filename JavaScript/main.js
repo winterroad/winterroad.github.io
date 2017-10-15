d0b547949bbb6c53c7ba66a3e7ff6d7a33e2ac7e
wintterroad <winterroaduk@gmail.com>
2017-10-16 02:01:55 +0300

    Elementtien uudelleen nimeämistä.

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,9 +1,9 @@
-  function openSlideMenu(){
-    document.getElementById('side-menu').style.width = '250px';
+  function avaaSivuMenu(){
+    document.getElementById('sivu-menu').style.width = '250px';
     document.getElementById('main').style.marginLeft = '250px';
   }
 
-  function closeSlideMenu(){
-    document.getElementById('side-menu').style.width = '0';
+  function suljeSivuMenu(){
+    document.getElementById('sivu-menu').style.width = '0';
     document.getElementById('main').style.marginLeft = '0';
   }
